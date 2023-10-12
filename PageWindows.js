f58650593b8e81c9ff9fe8511d45bf04ef28c8af
Taureon <45183108+Taureon@users.noreply.github.com>
2023-10-12 15:24:03 +0200

    Update PageWindows.js

--- PageWindows.js
+++ PageWindows.js
@@ -1,5 +1,36 @@
 let instances = [],
-	highestZIndex = 0;
+	highestZIndex = 0,
+
+defaultStyles = {
+	main: {},
+	head: { height: '30px', padding: '5px' },
+	icon: { width: '16px', height: '16px' },
+	title: {},
+	button: { wdith: '16px', height: '16px', margin: '4px', 'margin-right': '0px' },
+	content: { width: '100%', height: '100%'},
+	buttonHolder: { 'padding-right': '4px' }
+},
+
+applyAttrbutes = (object, attributes) => {
+    for (let key in attributes) object[key] = attributes[key];
+    return object;
+},
+
+mergeObjects = objs => {
+	let result = {};
+	for (let obj of objs) applyAttrbutes(result, obj);
+	return result;
+},
+
+applyStyle = (element, style) => applyAttrbutes(element.style, style),
+
+createElement = (type, style = {}, attributes = {}, children = []) => {
+    let element = document.createElement(type);
+    applyStyle(element, style);
+    applyAttrbutes(element, attributes);
+    for (let child of children) element.append(child);
+    return element;
+};
 
 window.addEventListener('resize', () => {
 	for (let i = 0; i < instances.length; i++) {
@@ -10,7 +41,8 @@ window.addEventListener('resize', () => {
 
 class PageWindow {
 	static defaultWidth = 300;
-	static defaultheight = 200;
+	static defaultHeight = 200;
+	static defaultStyles = defaultStyles;
 
 	constructor ({
 		title,
@@ -19,16 +51,12 @@ class PageWindow {
 		x = window.innerWidth / 2,
 		y = window.innerHeight / 2,
 		width = defaultWidth,
-		height = defaultheight,
+		height = defaultHeight,
 		frozen = false,
 		resizable = false,
 		verticalOrigin = "center",
 		horizontalOrigin = "center",
 	}) {
-
-		this.icon = icon;
-		this.title = title;
-		this.tooltip = tooltip;
 
 		this.X = x;
 		this.Y = y;
@@ -41,19 +69,37 @@ class PageWindow {
 
 		this.isDestroyed = false;
 
-		//TODO: actually do stuff with html elements
+		this.elementButtons = [];
+		for (let { icon, tooltip, click } of buttons) {
+			this.elementButtons.push(createElement('div', defaultStyles.button, {
+				title: tooltip,
+				onclick: click
+			}), [ icon ]);
+		}
+		this.elementButtonHolder = createElement('div', defaultStyles.buttonHolder, this.elementButtons);
+		this.elementContent = createElement('div', defaultStyles.content);
+		this.elementTitle = createElement('div', defaultStyles.title, { innerText: title });
+		this.elementIcon = createElement('div', defaultStyles.icon, {}, [ icon ]);
+		this.elementHead = createElement('div', defaultStyles.head, { /*make draggable*/ }, [ this.elementIcon, this.elementTitle, this.elementButtonHolder ]);
+		this.elementMain = createElement('div', mergeObjects(defaultStyles.main, { 'z-index': highestZIndex++ }), {}, [ this.elementHead, this.elementContent ]);
 
 		instances.push(this);
 	}
-	set x(x) {
-		this.X = x;
-	}
-	set y(y) {
-		this.Y = y;
-	}
+
+	set x(x) { this.X = x; }
+	set y(y) { this.Y = y; }
+
 	destroy() {
 		if (this.isDestroyed) throw new Error('Cannot destroy an already destroyed PageWindow.');
 		this.isDestroyed = true;
+
+		for (let button of this.elementButtons) button.remove();
+		this.elementButtonHolder.remove();
+		this.elementContent.remove();
+		this.elementTitle.remove();
+		this.elementIcon.remove();
+		this.elementHead.remove();
+		this.elementMain.remove();
 
 		instance.splice(instance.indexOf(this), 1);
 	}
